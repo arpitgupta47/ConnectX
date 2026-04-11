35d95e266b6fb5791b6023a008319ed2b7cc88ae
ARPIT GUPTA <arpit912524@gmail.com>
2026-04-11 16:16:12 +0530

    Ready for Deployement

--- backend/src/routes/app.js
+++ backend/src/routes/app.js
@@ -22,9 +22,11 @@ connectToSocket(server);
 const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
+    "http://localhost:5175",
     "http://localhost:5180",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "http://127.0.0.1:5175",
     "http://127.0.0.1:5180",
     "https://syncora-95py.onrender.com"
 ];
