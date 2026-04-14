7cbceb43d661b734a3b8bbfc48589ce321b8ef21
ARPIT GUPTA <arpit912524@gmail.com>
2026-04-15 01:11:03 +0530

    Update withAuth.jsx

--- vite-project/src/utils/withAuth.jsx
+++ vite-project/src/utils/withAuth.jsx
@@ -1,27 +1,15 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom"
+import { Navigate } from "react-router-dom";
 
-const withAuth = (WrappedComponent ) => {
-    const AuthComponent = (props) => {
-        const router = useNavigate();
+const withAuth = (WrappedComponent) => {
+  return (props) => {
+    const token = localStorage.getItem("token");
 
-        const isAuthenticated = () => {
-            if(localStorage.getItem("token")) {
-                return true;
-            } 
-            return false;
-        }
-
-        useEffect(() => {
-            if(!isAuthenticated()) {
-                router("/auth")
-            }
-        }, [])
-
-        return <WrappedComponent {...props} />
+    if (!token) {
+      return <Navigate to="/auth" replace />;
     }
 
-    return AuthComponent;
-}
+    return <WrappedComponent {...props} />;
+  };
+};
 
 export default withAuth;
