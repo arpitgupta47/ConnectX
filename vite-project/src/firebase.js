328ae762d22a2ff91a13acc1995744e1271b477f
ARPIT GUPTA <arpit912524@gmail.com>
2026-04-22 15:04:14 +0530

    Update firebase.js

--- vite-project/src/firebase.js
+++ vite-project/src/firebase.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7iBl0aaPkVFMSq4HQSCq7iHeysPbrU6I",
+  authDomain: "connectx-2454c.firebaseapp.com",
+  projectId: "connectx-2454c",
+  storageBucket: "connectx-2454c.firebasestorage.app",
+  messagingSenderId: "1057885985764",
+  appId: "1:1057885985764:web:dad24cf5eca770e31105f1"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
 
