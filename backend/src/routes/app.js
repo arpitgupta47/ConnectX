7b21a6800ca4047e84d07c440a4558a2e5c97e27
ARPIT GUPTA <arpit912524@gmail.com>
2026-04-25 09:26:07 +0530

    Add 'x-session-id' to CORS headers

--- backend/src/routes/app.js
+++ backend/src/routes/app.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
     if (origin) res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, x-session-id");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     if (req.method === "OPTIONS") return res.sendStatus(200);
     next();
